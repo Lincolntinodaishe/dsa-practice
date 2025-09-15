50e3529ade4b768282704125308eb9ee8e22d53d
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-09-14 23:48:44 -0500

    Time: 7 ms (81.68%), Space: 17.9 MB (95.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # METHOD1 appending to a new string the check the new string 
         # s = s.lower()
         # p =""
         # for c in s:
@@ -14,20 +15,40 @@ class Solution:
         #     r-=1
         # return True
 
+
+        #METHOD2 GIVING IF STATEMENTS FOR EACH AND EVERY CONDITION but with no extra space
+
+        # s = s.lower()
+        # l = 0
+        # r =len(s)-1
+        # while l<r:
+        #     if s[l].isalnum() and not s[r].isalnum():
+        #         r-=1
+        #     elif s[r].isalnum() and not s[l].isalnum():
+        #         l+=1
+        #     elif not s[r].isalnum() and not s[l].isalnum():
+        #         r-=1
+        #         l+=1
+        #     elif (s[r].isalnum() and s[l].isalnum()) and s[l] != s[r]:
+        #         return False
+        #     else:
+        #         r -=1
+        #         l +=1
+        # return True
+
+        #mETHOD 3 Using while loops with no extra space
+
+          
         s = s.lower()
         l = 0
         r =len(s)-1
         while l<r:
-            if s[l].isalnum() and not s[r].isalnum():
-                r-=1
-            elif s[r].isalnum() and not s[l].isalnum():
+            while l<r and not s[l].isalnum():
                 l+=1
-            elif not s[r].isalnum() and not s[l].isalnum():
+            while r>l and not s[r].isalnum():
                 r-=1
-                l+=1
-            elif (s[r].isalnum() and s[l].isalnum()) and s[l] != s[r]:
+            if s[l].lower() != s[r].lower():
                 return False
-            else:
-                r -=1
-                l +=1
+            l+=1
+            r-=1
         return True
