d84e90327f397a01c1c451146b9a05892a7139c5
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-09-18 03:05:57 -0500

    Time: 3 ms (37.01%), Space: 18 MB (24.74%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        # since arrays are not immutable we want to create a list of the chars
+        res = []
+        # now we loop at intervals that are 2*k long
+        # for eg 1 this is from a to d
+        for i in range(0, len(s), 2*k):
+            # for the first k chars we want to reverse them. so we append the then reverse them
+            res.append(s[i:i+k][::-1])
+            # for the rest of the terms in the interval we just append them
+            res.append(s[i+k:i+2*k])
+        # we join the items in the list to a string
+        return "".join(res)
