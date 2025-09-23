acab102fc7722e784dc8040eabb9c2256607dac4
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-09-23 08:58:49 -0500

    Time: 0 ms (100%), Space: 17.8 MB (78.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs = sorted(strs) # sort so that the two outer most words will be the most different
+        l = strs[0] # first word
+        r = strs[-1] # last word
+        res = ''
+        if strs == ['']:
+            return res
+        for i in range(min(len(strs[0]), len(strs[-1]))): # TRAVESE THROUGH THE LENGTH OF THE SHORTEST WORD
+            if l[i] != r[i]:
+                return res
+            res += l[i]
+        return res
+        
+        
