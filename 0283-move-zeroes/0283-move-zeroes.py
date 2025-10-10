c3edf36b5c36a28a2a6bcb3b0762fee07cfaecff
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-10-10 00:14:18 -0500

    Time: 3 ms (79.92%), Space: 18.9 MB (49.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
 
         """
+        # Use the one liner python swap
         k =0
         for i in range(len(nums)):
             if nums[i] != 0:
