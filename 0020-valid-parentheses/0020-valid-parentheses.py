0cd890b6159917d2cda6ecc490f2687c69321c42
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-10-09 23:05:59 -0500

    Time: 0 ms (100%), Space: 17.8 MB (56.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,23 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dict1 = {')': "(",
-                '}' :'{',
-                "]" : '['  }
+        #Brute
+        #  check if a combination is in the list of the pranhtess
+        # in a valid parenthis, thers always one true combination
+        # replace that combination with empty
+        # return s ==""
+        # each time replace runs it is O(n) time and it ends up being o(n2+)
+        # while "()" in s or "{}" in s or "[]" in s:
+        #   s = s.replace("()", "")
+        #   s = s.replace("{}", "")
+        #   s = s.replace("[]", "")
+        # return s == ""
+        
+
+        # Optimal
+        dict1 = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
         stack = []
-        for i in s: # ON
+        for i in s:
             if i in '({[':
                 stack.append(i)
-            if  i in ')}]':
-                if not stack or stack[-1] != dict1[i]:
+            if i in ')}]' :
+                if not stack or dict1[i] != stack[-1]:
                     return False
-                else: stack.pop()
+                stack.pop()
         return not stack
 
-        # O(N) time
-        # o(n) space
-      
-          
 
-
-
-          
