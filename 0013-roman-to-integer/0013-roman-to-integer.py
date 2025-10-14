1d9ab9b6cd159dafdfa7e1fbf0fae7580c0f8f82
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-10-14 00:21:23 -0500

    Time: 7 ms (41.76%), Space: 17.7 MB (64.51%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -10,7 +10,9 @@ class Solution:
             'M': 1000
         }
         res = 0
+        # reverse the string
         # s = s[::-1]
+        # loop upto the char before the last one because you cant compare after that as you will be out of range
         # for i in range(0, len(s)-1):
         #     if s[i] == 'V' and s[i+1] == 'I':
         #         res += 3
@@ -26,15 +28,19 @@ class Solution:
         #         res += 800
         #     else:
         #         res += dict1[s[i]]
+        # remember to add the last character after the loop
         # res += dict1[s[-1]]
         # return res
 
-        
+        # you dont need to reverse your string
+        # understand that if my current roman numeral is less than the one in front
+        # i have to subtract it
+        # you access evry char but the last because you dont what to go out of range when comparing the last cha
         for i in range(len(s)-1):  
             if dict1[s[i]] < dict1[s[i+1]]:
                 res -= dict1[s[i]]
             else:
                 res += dict1[s[i]]
-            
+         # add your last element to the result   
         res += dict1[s[-1]]
         return res
