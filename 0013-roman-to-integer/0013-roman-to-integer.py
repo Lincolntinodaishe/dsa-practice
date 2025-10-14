eafabdca0e6a60bdb9bdf6cef68ac2dab18f37fe
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-10-14 00:16:04 -0500

    Time: 5 ms (52.35%), Space: 17.8 MB (64.51%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -10,21 +10,31 @@ class Solution:
             'M': 1000
         }
         res = 0
-        s = s[::-1]
-        for i in range(0, len(s)-1):
-            if s[i] == 'V' and s[i+1] == 'I':
-                res += 3
-            elif s[i] == 'X' and s[i+1] == 'I':
-                res += 8
-            elif s[i] == 'L' and s[i+1] == 'X':
-                res += 30
-            elif s[i] == 'C' and s[i+1] == 'X':
-                res += 80
-            elif s[i] == 'D' and s[i+1] == 'C':
-                res += 300
-            elif s[i] == 'M' and s[i+1] == 'C':
-                res += 800
+        # s = s[::-1]
+        # for i in range(0, len(s)-1):
+        #     if s[i] == 'V' and s[i+1] == 'I':
+        #         res += 3
+        #     elif s[i] == 'X' and s[i+1] == 'I':
+        #         res += 8
+        #     elif s[i] == 'L' and s[i+1] == 'X':
+        #         res += 30
+        #     elif s[i] == 'C' and s[i+1] == 'X':
+        #         res += 80
+        #     elif s[i] == 'D' and s[i+1] == 'C':
+        #         res += 300
+        #     elif s[i] == 'M' and s[i+1] == 'C':
+        #         res += 800
+        #     else:
+        #         res += dict1[s[i]]
+        # res += dict1[s[-1]]
+        # return res
+
+        
+        for i in range(len(s)-1):  
+            if dict1[s[i]] < dict1[s[i+1]]:
+                res -= dict1[s[i]]
             else:
                 res += dict1[s[i]]
+            
         res += dict1[s[-1]]
         return res
