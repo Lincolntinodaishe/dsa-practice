f8d20631af62d88d86caf154d3d7cba59f5a3e31
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-10-14 00:55:10 -0500

    Time: 0 ms (100%), Space: 17.6 MB (83.34%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,6 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        # control i manually by using a while loop
+        # exchange the numbers with the last one uing python exchange
+        # and move the pointer
         i =0
         while i < len(nums):
             if nums[i] == val:
