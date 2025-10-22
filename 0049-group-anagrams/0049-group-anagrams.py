8fd7802d5c8514fd1c86ba63cfd55bdabc124dd7
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-10-22 00:46:33 -0500

    Time: 23 ms (18.86%), Space: 22.6 MB (24.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = defaultdict(list)
-        for i in strs:
-            count = [0]*26
-            for c in i:
-                count[ord(c)- ord('a')] += 1
-            dic[tuple(count)].append(i)
-        return list(dic.values())
+        # create a dictionary that stores lists of the words
+        dict1  = defaultdict(list)
+        
+        for i in strs: # iterate into each and evry word
+            count = [0]*26 # for each word i want to find the count of each letter at every position
+            for c in i: #for every character in the word
+                count[ord(c)-ord('a')] += 1 # cal its specific position by subtractin the ord('a') from each character to get a unique position
+            dict1[tuple (count)].append(i) # the key being specific to each word , i want to append the word but lists are not hashable hence i convert them to tuples
+        return list (dict1.values()) # I WANT TO RETURN THE LIST OF LISTS THEREFORE I RETURN THE VALUES THEN CONVERT THEM TO LISTS SINCE THEY ARE TUPLES AT THIS POINT
