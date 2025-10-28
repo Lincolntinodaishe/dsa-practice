26e7096abe897c840ac5495e5ad532b365ef0491
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-10-27 22:50:51 -0700

    Time: 0 ms (100%), Space: 17.7 MB (91.31%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # Transpose by swapping elements not in the diagonal
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # now swap the elements at the ends by reflecting the elements along the middle line.
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+        return matrix
+
+        
