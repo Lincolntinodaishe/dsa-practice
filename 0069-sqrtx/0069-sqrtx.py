00ab18de4961c82d8d679dca7588ed1a8e21ceba
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-11-02 21:05:52 -0800

    Time: 5 ms (34.06%), Space: 17.8 MB (50.97%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,5 +1,18 @@
 class Solution:
-    import math
     def mySqrt(self, x: int) -> int:
-        
-        return int (math.sqrt(x))
+        if x<2:
+            return x
+        ans = 0
+        l = 0
+        r = x
+        while l <= r:
+            m = (l+r)//2
+            if m * m == x:
+                return m
+            elif m * m < x:
+                ans = m # store m as the largest integer that can be squared to give a value less than x
+                l = m+1
+            else:
+                r = m-1 
+        return ans
+        # binarry search
