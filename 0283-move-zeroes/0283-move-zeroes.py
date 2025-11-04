853854cd01a8be088abc512845e8f7f949f01f28
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-11-04 01:34:31 -0800

    Time: 3 ms (79.99%), Space: 19 MB (31.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,7 +5,7 @@ class Solution:
         """
         l= 0
         for i in range(len(nums)):
-            if nums[i]!= 0 and nums[l] == 0:
+            if nums[i]!= 0:
                 nums[i], nums[l] = nums[l], nums[i]
                 l+=1
             else:
