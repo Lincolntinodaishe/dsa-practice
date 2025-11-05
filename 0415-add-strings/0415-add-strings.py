f7fbc3d763492888ddf35d1f99e215c8aa5bc287
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-11-04 19:49:14 -0800

    Time: 3 ms (89.85%), Space: 18.2 MB (37.78%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,26 +1,18 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        # sole like we are doing the normal addition on paper ie: start from the last number to the first
-        # create a storing list
-        res=[]
-        # intiate carry 0
-        carry = 0 
-        # pointers from the end fof the strings
+        # convert into lists
+        res = []
+        carry = 0
         i = len(num1)-1
         j = len(num2)-1
-        # while loop such that it keeps runing up until bot lists are done and we had nothing to carry
-        while j>=0 or i>=0 or carry > 0:
-            # create condition for the strings to give 0 when the pointers are ngtv
-            digit1 = int(num1[i]) if i >= 0 else 0
-            digit2 = int(num2[j]) if j >= 0 else 0
-
-            total = digit1 + digit2 + carry
-            # calc the carry using // which gives the anwer with no remainder
-            carry = (total // 10)
-            # append the actual number we need in the result 
-            res.append(str(total % 10))
-            # move pointers back
-            i -= 1
-            j -= 1
-        # since we have intergers already we now join them into a list but reversed
-        return "".join(res[::-1])
+        while i>=0 or j>=0 or carry:
+                val1 = int(num1[i]) if i>=0 else 0
+                val2 = int(num2[j]) if j>=0 else 0
+                total = val1 + val2 + carry
+                val = total%10
+                carry = total//10
+                res.append(str(val))
+                i -= 1
+                j -= 1
+        return "".join(res[::-1])        
+    
