38cc8d16f712fb993aca170d402f210a9504e6c2
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-12-03 02:13:09 -0800

    Time: 4 ms (10.73%), Space: 18.1 MB (38.62%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -2,12 +2,12 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
         for i in operations:
-            if (i.lstrip('-')).isdigit(): #function .lstrip us for negative integers to be tested .isdigit
-                stack.append(int(i))
-            elif i == '+':
+            if i == '+':
                 stack.append(stack[-1] + stack[-2])
             elif i == 'D':
                 stack.append(2*stack[-1])
             elif i == 'C':
                 stack.pop()
+            else:
+                stack.append(int(i))
         return sum(stack)
