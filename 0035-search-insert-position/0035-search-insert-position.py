17ece58fbb379a44ec37a8952862107484d4bd07
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-02-23 22:23:00 -0800

    Time: 0 ms (100%), Space: 20.2 MB (10.27%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,5 +1,6 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        '''
         #Brute force O(n)
         for i, n in enumerate(nums):
             if target == n:   #if target is n
@@ -7,5 +8,18 @@ class Solution:
             elif n > target:   #if n> target
                 return i
         return len(nums)       #otherwise
+        '''
+        # Binary Search
+        l = 0
+        r = len(nums)-1
 
+        while r>=l:
+            mid = (r+l)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                r =mid-1
+            else:
+                l = mid+1
+        return l
         
