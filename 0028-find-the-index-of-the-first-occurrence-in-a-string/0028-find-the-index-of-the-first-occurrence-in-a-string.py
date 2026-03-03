c8588ed574eb259bc0ca74bc6b09324c26fe960f
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-03-02 22:51:58 -0600

    Add two-pointer method for substring search
    Implemented a two-pointer approach to find the index of the first occurrence of a substring in a string.

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -8,3 +8,32 @@ class Solution:
             else:
                 continue
         return -1
+# two-pointer
+        '''
+        use 2 pointers
+        l - hay
+        r - need
+        search hay and need
+        if the pointers have the same value
+        move to the next
+        if not keep the hay pointer wher it is and move needle
+         pointer back to zero
+
+        '''
+        index = 0
+        l = 0
+        r = 0
+        while l<len(haystack) and r<len(needle):
+            if haystack[l] ==  needle[r]:  
+                
+                if r == len(needle)-1: #when we reach the last word in the needle return index
+                    return l-r
+                l+=1 #update pointers
+                r+=1
+                
+            else:  #if pointers are not equal, restart the needle and move the haystack
+                l= l-r+1
+                r = 0
+        return -1 # ifyou get out of the loop without finding anything return -1
+
+
