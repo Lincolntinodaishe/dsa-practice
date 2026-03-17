bc47c058671c654cf6bd0a5d75913a40b7e05dd4
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-03-16 22:35:37 -0500

    Comment out makeGood function in 1544-make-the-string-great.py
    Commented out the makeGood function for future reference.

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -10,4 +10,15 @@ class Solution:
                 stack.append(i)
         return ''.join(stack)
 
+# def makeGood(self, s: str) -> str:
+#         stack = []
+
+#         for ch in s:
+#             if stack and abs(ord(stack[-1]) - ord(ch)) == 32:
+#                 stack.pop()
+#             else:
+#                 stack.append(ch)
+
+#         return "".join(stack)
+
         
