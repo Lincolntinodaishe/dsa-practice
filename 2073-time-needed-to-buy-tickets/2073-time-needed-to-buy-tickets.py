1985efcae38d195c6b7e65504e001421596232ca
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-03-16 22:29:10 -0700

    Time: 0 ms (100%), Space: 19.4 MB (56.94%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,16 +1,27 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        d = {}
-        sec = 0
-        for i, n in enumerate(tickets):
-            d[i] = n
-        while d[k] > 0:
-            for i in range(len(tickets)):
-                if d[i] > 0:
-                    d[i] = d.get(i, 0)-1
-                    sec +=1
-                if d[k] == 0:
-                    return sec
-        return sec
+        # Optimal solution thats which is not a simulation
+        res = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                res += min(tickets[i], tickets[k])
+            else:
+                res += min(tickets[i], tickets[k]-1)
+        return res
+        # the numbers before ind k and the numbers after ind k
+
+        # my solution
+        # d = {}
+        # sec = 0
+        # for i, n in enumerate(tickets):
+        #     d[i] = n
+        # while d[k] > 0:
+        #     for i in range(len(tickets)):
+        #         if d[i] > 0:
+        #             d[i] = d.get(i, 0)-1
+        #             sec +=1
+        #         if d[k] == 0:
+        #             return sec
+        # return sec
 
         # time o(n)
