670ad94293faa5b20816c17fb880d3d7ca603336
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-03-19 21:36:48 -0700

    Time: 71 ms (16.44%), Space: 28.8 MB (20.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #bRUTE
-        # O(n2) time complexity looping twice in one array
-        max_profit = 0
+        # #Brute
+        # max_profit = 0
         # for i in range(len(prices)):
         #     for j in range(i+1, len(prices)):
-        #         profit = prices[j]-prices[i]
-        #         if profit > max_profit:
-        #             max_profit = profit
+        #         profit = prices[j] - prices[i]
+        #         max_profit = max(profit, max_profit)
         # return max_profit
+        
+        #sliding window
         l = 0
+        max_profit = 0
         r = 1
         while r < len(prices):
             if prices[r] > prices[l]:
                 profit = prices[r] - prices[l]
-                max_profit = max(profit, max_profit)
-            else: l = r
-            r += 1
+                max_profit = max(profit, max_profit)                  
+            else:
+                l= r
+            r+=1 
         return max_profit
