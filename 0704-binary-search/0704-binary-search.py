5bf5cd5cd7b2e37fcc0aa60cbfd1148743b6e3ec
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-03-25 20:05:38 -0700

    Time: 0 ms (100%), Space: 20.4 MB (98.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,12 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
+        r = len(nums)-1
+        l = 0
         while l<=r:
-            m = (r+l)//2  #look for the mid point
-            if target == nums[m]: 
-                return m
-            elif target < nums[m]: 
-                r = m-1
+            mid = (r+l)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid-1
             else:
-                l = m+1
+                l = mid+1
         return -1
+        
