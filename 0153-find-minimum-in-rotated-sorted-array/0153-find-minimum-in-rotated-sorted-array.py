2c80b51da3d76ecea8186bcb35bab04a812c3f50
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-03-31 22:00:33 -0500

    Time: 0 ms (100%), Space: 19.4 MB (47.76%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,35 +1,18 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # o(n)
-        # return min(nums)
-
-
-        # # two poniter
-        # res = float("inf")
-        # l = 0
-        # r = len(nums)-1
-        # while r >= l:
-        #     if nums[r] < nums[l]:
-        #         res = min(nums[r], res)
-        #         l += 1
-        #     else:
-        #         res =min(nums[l], res)
-        #         r-=1
-        # return res
-
-        #O(logn)
+        l = 0
+        r = len(nums)-1
         res = nums[0]
-        r, l = len(nums)-1, 0
         while l <= r:
-            if nums[l] < nums[r]:
-                res = min(res, nums[l])
+            if nums[l]<nums[r]:
+                res = min(nums[l], res)
                 break
-            m = (r+l)//2 #find the midpoint
-            res = min(res, nums[m])
-            if nums[m] >= nums[l]: # if the midpoint num is greater that the number at the begng it means the min value is to the right side
-                l = m+1
-            else: #if midpoint num is not greater than the num at l focus on the left
-                r = m-1
+            mid = (l+r)//2
+            res = min(res, nums[mid])
+            if nums[mid] >= nums[l]:
+                l = mid+1
+            else:
+                r = mid-1
         return res
 
         
