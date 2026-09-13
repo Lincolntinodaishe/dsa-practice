cf41327f0164d755c6f46628899b34ec430eb67f
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2026-09-12 23:18:07 -0500

    Time: 0 ms (100%), Space: 19.2 MB (73.28%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,13 +1,20 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         strs = sorted(strs) # sort so that the two outer most words will be the most different
+        
         l = strs[0] # first word
         r = strs[-1] # last word
         res = ''
+        '''
         for i in range(min(len(strs[0]), len(strs[-1]))): # TRAVESE THROUGH THE LENGTH OF THE SHORTEST WORD
             if l[i] != r[i]:
                 return res
             res += l[i]
         return res
+        '''
         
-        
+        for i in range(min (len(l), len(r))):
+            if l[i] != r[i]:
+                return res
+            res += l[i]
+        return res
